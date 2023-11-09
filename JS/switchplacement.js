2619813cda3f0dbb22976574369919496d80ca6a
MirageM <imiragesz@gmail.com>
2023-11-09 11:03:17 -0500

    case 'first': case 'second': case 'third': default

---
+++ JS/switchplacement.js
@@ -0,0 +1,15 @@
+var place = 'first'
+
+switch(place){
+    case 'first':
+        console.log('Gold medal');
+        break;
+    case 'second':
+        console.log('Silver medal');
+        break;
+    case 'third':
+        console.log('Bronze medal');
+        break;
+    default:
+        console.log('No medal');
+}
