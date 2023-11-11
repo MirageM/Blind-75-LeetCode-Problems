812fe74051d3bca84710bbe79160bdf8f06705fa
MirageM <imiragesz@gmail.com>
2023-11-10 23:25:49 -0500

    typeof(function abc(){console.log('abc');});

---
+++ JS/TypeOf.js
@@ -0,0 +1,32 @@
+// Using typeof
+
+var test1 = typeof('what is this?');
+var test2 = typeof(10);
+var test3 = typeof(3.14);
+var test4 = typeof(true);
+var test5 = typeof(false);
+var test6 = typeof(1 < 2);
+var test7 = typeof([1, 2, 3]);
+var test8 = typeof({firstProperty: 1});
+var test9 = typeof({firstProperty: 1});
+var test10 = typeof(function abc(){console.log('abc');});
+var test11 = typeof(null);
+var test12 = typeof(undefined);
+var test13 = typeof(NaN);
+var test14 = typeof(Infinity);
+var test15 = typeof(-Infinity);
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+console.log(test5);
+console.log(test6);
+console.log(test7);
+console.log(test8);
+console.log(test9);
+console.log(test10);
+console.log(test11);
+console.log(test12);
+console.log(test13);
+console.log(test14);
+console.log(test15);
