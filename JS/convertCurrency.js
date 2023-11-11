cf7924054d442f78d3b8c764f986d52923aa13ad
MirageM <imiragesz@gmail.com>
2023-11-11 16:30:27 -0500

    currencyTwo = convertCurrency(currencyOne, exchangeRate);

---
+++ JS/convertCurrency.js
@@ -0,0 +1,12 @@
+// functional programming
+
+var currencyOne = 100;
+var currentTwo = 0;
+var exchange = 1.2;
+
+function convertCurrency(amount, rate){
+    return amount * rate;
+}
+
+currencyTwo = convertCurrency(currencyOne, exchangeRate);
+console.log(currentTwo);
