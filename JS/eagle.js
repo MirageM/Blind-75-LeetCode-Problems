471be240198c767f7e42d321488c7e6fc4c9ee07
MirageM <imiragesz@gmail.com>
2023-11-11 19:30:07 -0500

    var eagle1 = Object.create(bird);

---
+++ JS/eagle.js
@@ -0,0 +1,12 @@
+var bird = {
+    hasWings: true,
+    canFly: true,
+    hasFeathers: true
+}
+
+var eagle1 = Object.create(bird);
+console.log("eagle1: ", eagle1);
+
+console.log("eagle1 has wings:", eagle1.hasWings);
+console.log("eagle1 can fly:", eagle1.canFly);
+console.log("eagle1 has feathers:", eagle1.hasFeathers);
