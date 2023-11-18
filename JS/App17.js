1f7a4427080cd28b627293de7ca68c2c8a79a6b6
MirageM <imiragesz@gmail.com>
2023-11-18 17:20:40 -0500

    onClick = {decrement} onClick = {increment}

---
+++ JS/App17.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+function App(){
+	const[count, setCount] = useState(4);
+	function decrement(){
+		setCount(count - 1);
+	}
+	function increment(){
+		setCount(count + 1);
+	}
+	<>
+	<button onClick = {decrement}>-</button>
+	<span>{count}</span>
+	<button onClick = {increment}>+</button>
+	</>
+
+}
+
+export default App;
